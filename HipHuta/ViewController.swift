f501960f0fbd91045b61314a9721a5a0b86a03b5
SebaKrk <s.sciuba@gmail.com>
2020-11-25 13:42:49 +0100

    create a attributedText and add new text to descripctionTextView + set.new cons

--- HipHuta/ViewController.swift
+++ HipHuta/ViewController.swift
@@ -18,33 +18,34 @@ class ViewController: UIViewController {
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
+    
     var descripctionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Hip Huta"
-        textView.font = UIFont.boldSystemFont(ofSize: 70)
+        let attributedText = NSMutableAttributedString(string: "Hip Huta", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 70)])
+
+        attributedText.append(NSAttributedString(string: "\nJedz, baw się i zakochaj w Nowej Hucie", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 30),NSAttributedString.Key.foregroundColor:UIColor.gray]))
+
+        textView.attributedText = attributedText
+        textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textAlignment =  .center
         textView.textColor = .black
-        textView.translatesAutoresizingMaskIntoConstraints = false
         
         return textView
     }()
     
     
-    
-//    "Hip Huta"
-//    "Jedz, baw się i zakochaj w Nowej Hucie"
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpTopStackView()
+  
+    
     }
     
     func setUpTopStackView() {
         
         let topImageConteinerView = UIView()
-        topImageConteinerView.backgroundColor = .blue
+        //        topImageConteinerView.backgroundColor = .blue
         view.addSubview(topImageConteinerView)
         
         
@@ -53,21 +54,21 @@ class ViewController: UIViewController {
         topImageConteinerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         topImageConteinerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         topImageConteinerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
-    
+        
         topImageConteinerView.addSubview(logoImageView)
-    
+        
         logoImageView.centerXAnchor.constraint(equalTo: topImageConteinerView.centerXAnchor).isActive = true
         logoImageView.centerYAnchor.constraint(equalTo: topImageConteinerView.centerYAnchor).isActive = true
         logoImageView.heightAnchor.constraint(equalTo: topImageConteinerView.heightAnchor, multiplier: 0.85).isActive = true
         
-        view.addSubview(descripctionTextView)
-
+        view.addSubview(descripctionTextView) // tekst
         descripctionTextView.topAnchor.constraint(equalTo: topImageConteinerView.bottomAnchor).isActive = true
-        descripctionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descripctionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descripctionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
+        descripctionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
         descripctionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
     }
-
+    
 }
 
 
