ebe2c5a63fb4a0feadfb11e73e55d76a6983602b
SebaKrk <s.sciuba@gmail.com>
2020-11-25 07:58:32 +0100

    created UIImageView = logoImage and set constreins

--- HipHuta/ViewController.swift
+++ HipHuta/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    var logoImageView : UIImageView = {
+        var imageView = UIImageView(image: #imageLiteral(resourceName: "HipHutaLogo"))
+        imageView.adjustsImageSizeForAccessibilityContentSizeCategory = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(logoImageView)
+        logoImageView.frame = view.frame
+        
     }
 
 
 }
+
+
+//https://www.youtube.com/watch?v=Hn_MGvuYZdg
 
