1b29011aacd52f33251a50bb276a3d53f0d62e35
SebaKrk <s.sciuba@gmail.com>
2020-11-26 11:28:48 +0100

    change font size and add constraints

--- HipHuta/AboutAsViewController.swift
+++ HipHuta/AboutAsViewController.swift
@@ -11,18 +11,18 @@ class AboutAsViewController: UIViewController {
     
     var descripctionTextView : UITextView = {
         let textView = UITextView()
-        let attributedText = NSMutableAttributedString(string: "Hip Huta", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 30)])
+        let attributedText = NSMutableAttributedString(string: "Hip Huta", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 70)])
         
-        attributedText.append(NSAttributedString(string: "\nJedz, baw się i zakochaj w Nowej Hucie", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 20),NSAttributedString.Key.foregroundColor:UIColor.black]))
+        attributedText.append(NSAttributedString(string: "\nJedz, baw się i zakochaj w Nowej Hucie", attributes:
+            [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 30)]))
         
-        attributedText.append(NSAttributedString(string: "\nBlog poświęcony kawiarniom, restauracjom i fajnym inicjatywom w najmłodszej dzielnicy Krakowa. Dołącz do nas i pokaż, że NH jest „hip”!\nUważasz, że powinniśmy odwiedzić twój lokal? Znalazłeś ciekawe miejsce w Hucie o którym powinniśmy napisać? Chciałbyś z nami współpracować?\n'nKoniecznie daj nam o tym znać na hiphuta@gmail.com lub Facebooku. Odwiedź też naszego Instagrama", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 18),NSAttributedString.Key.foregroundColor:UIColor.gray]))
+        attributedText.append(NSAttributedString(string: "\n\n\nBlog poświęcony kawiarniom, restauracjom i fajnym inicjatywom w najmłodszej dzielnicy Krakowa. \nDołącz do nas i pokaż, że NH jest „hip”!\n\nUważasz, że powinniśmy odwiedzić twój lokal? Znalazłeś ciekawe miejsce w Hucie o którym powinniśmy napisać? Chciałbyś z nami współpracować?\n\nKoniecznie daj nam o tym znać na hiphuta@gmail.com lub Facebooku. \nOdwiedź też naszego Instagrama", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 20),NSAttributedString.Key.foregroundColor:UIColor.gray]))
         
         textView.attributedText = attributedText
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textAlignment =  .center
         textView.textColor = .black
         textView.isEditable = false
-        
         
         return textView
     }()
@@ -31,18 +31,19 @@ class AboutAsViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        title = "O Nas "
+        title = "O Nas"
         
+        setUpDescView()
+    }
+    
+    func setUpDescView() {
+        view.addSubview(descripctionTextView)
         
+        descripctionTextView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        descripctionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
+        descripctionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
+        descripctionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         
     }
 }
 
-
-//Jedz, baw się i zakochaj w Nowej Hucie! Blog poświęcony kawiarniom, restauracjom
-//i fajnym inicjatywom w najmłodszej dzielnicy Krakowa. Dołącz do nas i pokaż, że NH jest „hip”!
-//
-//Uważasz, że powinniśmy odwiedzić twój lokal? Znalazłeś ciekawe miejsce w Hucie o którym powinniśmy napisać? Chciałbyś z nami współpracować?
-//
-//Koniecznie daj nam o tym znać na hiphuta@gmail.com lub Facebooku. Odwiedź też naszego Instagrama!
-//
