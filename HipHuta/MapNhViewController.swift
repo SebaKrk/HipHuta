51cc56913f6ebd3c614f002601e04ec6619ce570
SebaKrk <s.sciuba@gmail.com>
2020-11-27 09:14:06 +0100

    crate func setUpContainerView and create a three conteiners

--- HipHuta/MapNhViewController.swift
+++ HipHuta/MapNhViewController.swift
@@ -18,11 +18,42 @@ class MapNhViewController : UIViewController {
         view.backgroundColor = .white
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "back" , style: .plain, target: self, action: #selector(dismisSelf))
         
+        setUpContainerView()
+        
+        
+        
     }
     @objc private func dismisSelf() {
         dismiss(animated: true, completion: nil)
     }
+    func setUpContainerView() {
+        let upperContainer = UIView()
+        view.addSubview(upperContainer)
+        
+        upperContainer.backgroundColor = .red
+        upperContainer.frame = CGRect(x: 0, y: 100, width: 100, height: 100)
+        
+        let centerContainer = UIView()
+        view.addSubview(centerContainer)
+        centerContainer.backgroundColor = .green
+        centerContainer.frame = CGRect(x: 0, y: 300, width: 100, height: 100)
+        
+        let bottomContainer = UIView()
+        view.addSubview(bottomContainer)
+        bottomContainer.backgroundColor = .blue
+        bottomContainer.frame = CGRect(x: 0, y: 500, width: 100, height: 100)
+        
+    }
+    
+    
 }
+
+
+
+
+
+
+
 
 
 // WASI ZNAJOMI TWIERDZĄ, ŻE W HUCIE NIE MA GDZIE ZJEŚĆ?
