11a7fb0322140b11aa700b65f539c21b048a5400
SebaKrk <s.sciuba@gmail.com>
2020-11-29 09:02:20 +0100

    create label and image view

---
+++ HipHuta/View/TheBestinNgCell.swift
@@ -0,0 +1,34 @@
+//
+//  TheBestinNgCell.swift
+//  HipHuta
+//
+//  Created by Sebastian Sciuba on 29/11/2020.
+//
+
+import UIKit
+
+class TheBestInNhCell: UICollectionViewCell {
+    
+    let nameLabel: UILabel = { // nazwa restauracji
+        let label = UILabel()
+     
+        return label
+    }()
+    
+    let imageView: UIImageView = { // zdjecie oceny restauracji
+        let iv = UIImageView()
+        
+        return iv
+    }()
+    
+    let secImageView: UIImageView = { // zdjecie restauracji
+        let iv = UIImageView()
+        
+        return iv
+    }()
+    
+    
+    
+    
+    
+}
