b1fd6de06950043cc6aff40a8d952360360ddf7f
SebaKrk <s.sciuba@gmail.com>
2020-11-29 08:27:39 +0100

    create view cell container with red background

--- HipHuta/TheBestInNhViewController.swift
+++ HipHuta/TheBestInNhViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class TheBestInNhViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let reuseIdentifier = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
+        collectionView.backgroundColor = .white
+        title = "The best in Nowa Huta"
+        collectionView.isPagingEnabled = true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 100)
     }
 }
    
