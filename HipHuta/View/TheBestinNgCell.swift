b9d459fc19695be0cf009b09dc64ff497a3d7f78
SebaKrk <s.sciuba@gmail.com>
2020-11-29 09:15:49 +0100

    create new container for views

--- HipHuta/View/TheBestinNgCell.swift
+++ HipHuta/View/TheBestinNgCell.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class TheBestInNhCell: UICollectionViewCell {
     
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        
+        view.addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        return view
+    }()
+    
     let nameLabel: UILabel = { // nazwa restauracji
         let label = UILabel()
-     
+        
+        label.text = "Restauracja 1"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16)
+        
         return label
     }()
     
@@ -27,6 +43,7 @@ class TheBestInNhCell: UICollectionViewCell {
         return iv
     }()
     
+  
     
     
     
