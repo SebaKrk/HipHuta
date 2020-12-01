e4538cc0113756ba3e6a7802f8208bdb8c6e2a38
SebaKrk <s.sciuba@gmail.com>
2020-12-01 15:43:47 +0100

    crate a init

--- HipHuta/View/RestaurantInfoView.swift
+++ HipHuta/View/RestaurantInfoView.swift
@@ -31,4 +31,17 @@ class RestaurantInfoView: UIView {
         label.text = "Restauracja"
         return label
     }()
+    
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
+
+
