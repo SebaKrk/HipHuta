09cf7bfc0e8626b3f3d0035c50117005ac2c7c59
SebaKrk <s.sciuba@gmail.com>
2020-12-12 07:04:37 +0100

    create new View controller - AboutRestaurantVC

---
+++ HipHuta/Controller/AboutRestaurant.swift
@@ -0,0 +1,28 @@
+//
+//  AboutRestaurant.swift
+//  HipHuta
+//
+//  Created by Sebastian Sciuba on 12/12/2020.
+//
+
+import UIKit
+
+class AboutRestaurantVC: UIViewController {
+    
+  
+    //    MARK: - ViewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "The Best in Noowa Huta"
+        
+        
+    }
+    
+    //    MARK: - VIEW Constraint
+    
+ 
+    
+}
